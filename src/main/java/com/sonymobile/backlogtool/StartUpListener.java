36d43fd245853b4a025c858ed485190c22f6bbaa
Fredrik Persson <fredrik-persson@outlook.com>
2013-06-20 10:24:11 +0200

    Created a simple framework for handling schema updates
    Classes in the dbupdate package that extend DbUpdater and have the
    annotation DbUpdate will get registered as schema updates and will
    be run at startup. The first update increases the length of story
    descriptions.

--- src/main/java/com/sonymobile/backlogtool/StartUpListener.java
+++ src/main/java/com/sonymobile/backlogtool/StartUpListener.java
@@ -26,9 +26,8 @@ package com.sonymobile.backlogtool;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
@@ -36,19 +35,24 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.stereotype.Component;
 
+import com.sonymobile.backlogtool.dbupdate.DbUpdate;
+import com.sonymobile.backlogtool.dbupdate.DbUpdater;
+import com.sonymobile.backlogtool.dbupdate.SchemaVersion;
 import com.sonymobile.backlogtool.permission.User;
 
 /**
  * This class listens for startup events and updates the master admin list
  * from the configuration file on server launch.
  *
- * @author Fredrik Persson &lt;fredrik5.persson@sonymobile.com&gt;
+ * @author Fredrik Persson &lt;fredrik6.persson@sonymobile.com&gt;
  */
 @Component
 public class StartUpListener implements ApplicationListener<ContextRefreshedEvent> {
@@ -59,9 +63,73 @@ public class StartUpListener implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {  
         updateMasterAdmins();
-        updateStoryDescription();
+        updateDatabaseSchema();
     }
-    
+
+    /**
+     * Checks for schema updates and applies them in order.
+     */
+    private void updateDatabaseSchema() {
+
+        SchemaVersion schemaVersion = null;
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Query query = session.createQuery("from SchemaVersion");
+            Object result = query.uniqueResult();
+            if (result instanceof SchemaVersion) {
+                schemaVersion = (SchemaVersion) result;
+            }
+            else {
+                //The table SchemaVersion did not exist; create new
+                schemaVersion = new SchemaVersion();
+                session.save(schemaVersion);
+            }
+
+            //Scans for all classes annotated with @DbUpdate
+            ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(true);
+            scanner.addIncludeFilter(new AnnotationTypeFilter(DbUpdate.class));
+            List<DbUpdater> updateList = new ArrayList<DbUpdater>();
+            for (BeanDefinition bd : scanner.findCandidateComponents("com.sonymobile.backlogtool.dbupdate")) {
+                try {
+                    DbUpdater updater = (DbUpdater) Class.forName(bd.getBeanClassName()).newInstance();
+                    updateList.add(updater);
+
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            //Attempt to run all updates in order
+            Collections.sort(updateList);
+            for (DbUpdater updater : updateList) {
+                if (updater.getFromVersion() == schemaVersion.getVersion()) {
+                    boolean success = updater.update(sessionFactory);
+                    String updateName = updater.getClass().getSimpleName();
+                    if (success) {
+                        System.out.println("Ran schema update " + updateName);
+                        System.out.println("New schema version " + updater.getToVersion());
+                        schemaVersion.setVersion(updater.getToVersion());
+                    } else {
+                        System.out.println("Failed to make schema update " + updateName);
+                    }
+                }
+            }
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            session.close();
+        }
+    }
+
     /**
      * Updates the master admin list.
      */
@@ -99,55 +167,7 @@ public class StartUpListener implements ApplicationListener<ContextRefreshedEven
                 }
             }
             tx.commit();
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (tx != null) {
-                tx.rollback();
-            }
-        } finally {
-            session.close();
-        }
-    }
-    
-    /**
-     * Update length of story description
-     * (since it was changed in version 1.1.6)
-     */
-    private void updateStoryDescription() {
-        Properties propertiesFile = new Properties();
-        try {
-            String backlogconf = System.getProperty("catalina.home") + File.separator + "conf/backlogtool.properties";
-            InputStream input = new FileInputStream(backlogconf);
-            propertiesFile.load(input);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        final String dialect = (String) propertiesFile.get("db.hibernate.dialect");
-        
-        Transaction tx = null;
-        Session session = sessionFactory.openSession();
-        try {
-            tx = session.beginTransaction();
 
-            session.doWork(new Work() {
-                @Override
-                public void execute(Connection connection) throws SQLException {
-                    String query = null;
-                    if (dialect.contains("PostgreSQL")) {
-                        query = "ALTER TABLE stories ALTER COLUMN description TYPE varchar(%d)";
-                    } else if (dialect.contains("HSQL")) {
-                        query = "ALTER TABLE stories ALTER COLUMN description varchar(%d)";
-                    }
-                    if (query != null) {
-                        query = String.format(query, Story.DESCRIPTION_LENGTH);
-                        connection.prepareStatement(query).executeUpdate();                        
-                    }
-                }
-            });
-
-            tx.commit();
-            
         } catch (Exception e) {
             e.printStackTrace();
             if (tx != null) {
@@ -158,3 +178,4 @@ public class StartUpListener implements ApplicationListener<ContextRefreshedEven
         }
     }
 }
+
