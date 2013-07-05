e36bcbb010168073bc1f2bdb21978bb1c958d3bd
Fredrik Persson <fredrik-persson@outlook.com>
2013-07-05 13:40:09 +0200

    Fixed that the header was sometimes overlapping the page content
    fixes sonyxperiadev/BacklogTool#100

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -2393,8 +2393,6 @@ $(document).ready(function () {
         setHeightAndMargin($("#header").height());
     });
 
-    setHeightAndMargin($("#header").height());
-
     /*
      * Changing the create parent button based on what view you're on
      * Also changing the view description text and the color of the view links
@@ -2656,5 +2654,7 @@ $(document).ready(function () {
         //   isShift = true;
         // }
     });
+
+    setHeightAndMargin($("#header").height());
 
 });
